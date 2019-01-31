4c45ac9c5805fa3ea751f1e8f39da84d1ad5c5ac
Kapil Goyal <32608819+kapilggg10@users.noreply.github.com>
2019-02-01 01:46:38 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,10 @@
+File structure:
+
+"files" :  directory contains training data. i.e Files containing text to train a model
+
+"plag_training.py" : is the file to create a model and return 'tf idf' , 'tfs' and 'files'. Here 'tf idf' is the vectorized and tokenized model of the training data 'tfs' is the matrix model of the data and 'files' is the list of all training files.
+
+
+
+"plagiarism.py" : is the file to check plagiarism between input file and trained data.
+
---
+++ plag_training.py
@@ -0,0 +1,49 @@
+from nltk.tokenize import word_tokenize 
+import os
+import string
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
+
+
+#function to tokenize the text file
+def get_tokenized(text):
+    tokens = word_tokenize(text)
+    return tokens
+
+
+#function to stemming tokens
+def stemming(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+
+def preprocessing(text):
+    tokens = get_tokenized(text) #tokenize
+    stemmer = PorterStemmer()
+    stem = stemming(tokens,stemmer) # stemming
+    return stem
+
+def training_tfs():
+    fs = []
+    i = 0
+    path = 'C:\\Users\\Kapil Goyal\\Desktop\\nlp_assignment\\files'
+    token_dictionary = {}
+    for subdir, dirs,files in os.walk(path):
+        for file in files:
+            file_path = subdir + os.path.sep + file
+            f = open(file_path,'r')
+            lowers = f.read().lower()
+            #lowers= text.lower()
+            no_punctuation = lowers.translate(string.punctuation)
+            token_dictionary[file] = no_punctuation
+            fs.append(file)# = file
+        
+    tfidf = TfidfVectorizer(tokenizer = preprocessing,stop_words = 'english')
+    tfs = tfidf.fit_transform(token_dictionary.values())
+    return tfidf,tfs,fs
+
